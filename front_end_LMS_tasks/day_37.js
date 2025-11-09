eb98437205e3cf4343baa34397ce850548f610c5
Muhammed Fahiz M <muhammedfahiz777145@gmail.com>
2025-11-09 16:41:07 +0530

    spacing

--- front_end_LMS_tasks/day_37.js
+++ front_end_LMS_tasks/day_37.js
@@ -29,7 +29,8 @@ let num = prompt("Enter a number:");
 num = Number(num);
 
 if (num > 0) {
-  console.log("The number is positive.");
+  console.log("The number is positive."); 
+  
 } else if (num < 0) {
   console.log("The number is negative.");
 } else {
