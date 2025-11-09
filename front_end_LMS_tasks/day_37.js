a6290b19dbc15ce2fb2a683b0793b8a83b19be4d
Muhammed Fahiz M <muhammedfahiz777145@gmail.com>
2025-11-09 16:37:44 +0530

    front end

---
+++ front_end_LMS_tasks/day_37.js
@@ -0,0 +1,44 @@
+// 1. Create a JavaScript file that: Declares variables for your name, age, and favorite hobby.
+// Logs a message using template literals: 'Hi, I’m [name], [age] years old, and I love [hobby].'
+//  Includes a calculation of two numbers and logs the result."
+
+
+// let name = "John";
+// let age = 20;
+// let hobby = "playing football";
+
+// console.log(`Hi, I'm ${name}, ${age} years old, and I love ${hobby}.`);
+
+// let num1 = 10;
+// let num2 = 5;
+// let sum = num1 + num2;
+
+// console.log("The sum is:", sum);
+
+
+
+
+// 1. Create a fully styled webpage using CSS animations, variables, and media queries.
+
+
+// 1. Write a program that checks if a number is positive, negative, or zero.
+
+// Program to check if a number is positive, negative, or zero
+
+let num = prompt("Enter a number:");
+num = Number(num);
+
+if (num > 0) {
+  console.log("The number is positive.");
+} else if (num < 0) {
+  console.log("The number is negative.");
+} else {
+  console.log("The number is zero.");
+}
+
+
+
+
+
+
+
